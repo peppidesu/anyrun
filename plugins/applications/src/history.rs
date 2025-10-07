59f64beb1c3eefdfcbb93b56c3b4edf4a0424cb2
peppidesu <bakker.pepijn@gmail.com>
2025-10-07 19:13:33 +0200

    Update history.rs

--- plugins/applications/src/history.rs
+++ plugins/applications/src/history.rs
@@ -1,9 +1,8 @@
 use std::collections::VecDeque;
-use std::fs;
 use std::env;
+use std::fs;
 
 use crate::scrubber::DesktopEntry;
-
 
 pub struct History(VecDeque<DesktopEntry>);
 
@@ -12,18 +11,18 @@ impl History {
         Self(VecDeque::new())
     }
 
-    pub fn load() -> Self {             
-
+    pub fn load() -> Self {
         let path = format!(
             "{}/.cache/anyrun-applications-history",
             env::var("HOME").expect("Unable to determine HOME directory")
         );
 
         if let Ok(content) = fs::read_to_string(&path) {
-            let mut history: VecDeque<DesktopEntry> = ron::from_str(&content).unwrap_or_else(|why| {
-                eprintln!("Error parsing history: {}", why);
-                VecDeque::new()
-            });            
+            let mut history: VecDeque<DesktopEntry> =
+                ron::from_str(&content).unwrap_or_else(|why| {
+                    eprintln!("Error parsing history: {}", why);
+                    VecDeque::new()
+                });
             return Self(history);
         }
 
@@ -31,7 +30,6 @@ impl History {
     }
 
     pub fn write(&self) {
-
         let path = format!(
             "{}/.cache/anyrun-applications-history",
             env::var("HOME").expect("Unable to determine HOME directory")
@@ -46,7 +44,6 @@ impl History {
         }
     }
     pub fn add_entry(&mut self, entry: DesktopEntry) {
-        
         self.0.push_back(entry);
     }
 
@@ -54,11 +51,9 @@ impl History {
         self.0.truncate(max_entries);
     }
 
-    pub fn get_entry_info(&self, entry: &DesktopEntry) -> Option<(usize, usize)> {        
+    pub fn get_entry_info(&self, entry: &DesktopEntry) -> Option<(usize, usize)> {
         let index = self.0.iter().position(|x| x == entry)?;
         let count = self.0.iter().filter(|x| *x == entry).count();
         Some((index, count))
     }
-
 }
-
